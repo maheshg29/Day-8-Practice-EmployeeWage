bb820d2e53bb07775f0558baa78628ff93e2a07a
mahesh <gangurdemahesh9@gmail.com>
2022-11-15 17:42:02 +0530

    [Mahesh]Add UC1 program and Employeewage file

---
+++ Day_8_Practice_EmployeeWage/EmployeeWage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8_Practice_EmployeeWage
+{
+    public class EmployeeWage
+    {
+        public void EmployeeDetails()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Not Present");
+            }
+        }
+    }
+}
+
+
+
--- Day_8_Practice_EmployeeWage/Program.cs
+++ Day_8_Practice_EmployeeWage/Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.EmployeeDetails();
         }
     }
 }
