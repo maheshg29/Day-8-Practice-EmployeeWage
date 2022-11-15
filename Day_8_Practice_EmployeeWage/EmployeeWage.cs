d8a51c1088d2f3cf076bccb3fdc744ba1943049d
mahesh <gangurdemahesh9@gmail.com>
2022-11-15 17:46:48 +0530

    [Mahesh]Add UC4 file

--- Day_8_Practice_EmployeeWage/EmployeeWage.cs
+++ Day_8_Practice_EmployeeWage/EmployeeWage.cs
@@ -8,34 +8,35 @@ namespace Day_8_Practice_EmployeeWage
 {
     public class EmployeeWage
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         public void EmployeeDetails()
         {
-            Console.WriteLine("Welcome to Employee Wage Computation");
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
 
-            if (empCheck == IS_FULL_TIME)
+            switch (empWage)
             {
-                //Console.WriteLine("Employee is Present");
-                empHrs = 8;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                // Console.WriteLine("Employee is Not Present");
-                empHrs = 0;
-            }
+
 
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("EMP Wage : " + empWage);
+
         }
     }
 }
