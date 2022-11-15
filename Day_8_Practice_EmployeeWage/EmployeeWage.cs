c0513c33c1a2a5aa2a1aed0ff662a1dd67f702f9
mahesh <gangurdemahesh9@gmail.com>
2022-11-15 17:50:23 +0530

    [Mahesh]Add UC6 file

--- Day_8_Practice_EmployeeWage/EmployeeWage.cs
+++ Day_8_Practice_EmployeeWage/EmployeeWage.cs
@@ -12,14 +12,18 @@ namespace Day_8_Practice_EmployeeWage
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_WORKING_DAY = 20;
+        public const int MAX_WORKING_HRS = 100;
         public void EmployeeDetails()
         {
             int empHrs = 0;
             int empWage = 0;
             int totalEmpWage = 0;
+            int totalemphrs = 0;
+            int totalworkingday = 0;
 
-            for (int day = 0; day < NUM_WORKING_DAY; day++)
+            while (totalemphrs <= MAX_WORKING_HRS && totalworkingday <= NUM_WORKING_DAY)
             {
+                totalworkingday++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
 
@@ -37,11 +41,11 @@ namespace Day_8_Practice_EmployeeWage
                         empHrs = 0;
                         break;
                 }
+                totalemphrs += empHrs;
 
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("EMP Wage : " + empWage);
+                Console.WriteLine("Day :" + totalworkingday + "emp hrs" + empHrs);
             }
+            totalEmpWage = totalemphrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Empolyee Wage :" + totalEmpWage);
 
         }
